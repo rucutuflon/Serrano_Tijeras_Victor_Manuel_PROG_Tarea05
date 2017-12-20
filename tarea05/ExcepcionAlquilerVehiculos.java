584dda9558e99235c99484309ca2dd8aeeceb3d6
rucut <rucut@192.168.1.45>
2017-12-20 16:57:02 +0100

    Commit ExcepcionAlquilerVehiculos

---
+++ tarea05/ExcepcionAlquilerVehiculos.java
@@ -0,0 +1,11 @@
+package tarea05;
+
+public class ExcepcionAlquilerVehiculos extends Exception {
+
+	private static final long serialVersionUID = 0;
+	
+	public ExcepcionAlquilerVehiculos (String mensaje) {
+		super(mensaje);
+	}
+
+}
