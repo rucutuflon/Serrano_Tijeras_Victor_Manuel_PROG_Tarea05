90284e223d11073979b9cb7f16662692bd345b89
rucut <rucut@192.168.1.45>
2017-12-20 17:27:16 +0100

    Primer commit clase Cliente

---
+++ tarea05/Cliente.java
@@ -0,0 +1,14 @@
+package tarea05;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Cliente {
+	private String nombre;
+	private String direccion;
+	private String localidad;
+	private String codigoPostal;
+	private int identificador;
+	private static int numClientes= 0;
+	
+}
