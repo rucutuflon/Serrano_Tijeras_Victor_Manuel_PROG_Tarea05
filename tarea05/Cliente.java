50cefb716b450d7b63686710696c36f6afbeb485
rucut <rucut@DESKTOP-NBA2QKJ>
2018-01-03 10:25:43 +0100

    Commit toString Cliente

--- tarea05/Cliente.java
+++ tarea05/Cliente.java
@@ -64,4 +64,10 @@ public class Cliente {
 	public static int getNumClientes() {
 		return numClientes;
 	}
+
+	public String toString() {
+		return "(" + nombre + ", " + direccion + ", " + localidad + ", " + codigoPostal + ", " + identificador + ")";
+	}
+	
+	
 }
