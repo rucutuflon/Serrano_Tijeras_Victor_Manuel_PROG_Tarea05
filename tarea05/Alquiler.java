614b4f8d11c563c77eb7b4430559bd6fca59242a
rucut <rucut@DESKTOP-NBA2QKJ>
2018-02-01 18:58:28 +0100

    Commit alquiler

---
+++ tarea05/Alquiler.java
@@ -0,0 +1,15 @@
+package tarea05;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Alquiler {
+	private Date fecha;
+	private int dias;
+	private final SimpleDateFormat FORMATO_FECHA = new SimpleDateFormat("dd-mm-yyyy");
+	private final int MS_DIA = 1;
+	private final double PRECIO_DIA = 2;
+	private Turismo turismo;
+	private Cliente cliente;
+	
+}
